63af0449539e3c104931a57b8f9a1b8633773fa2
Jeiler Vargas <jeiler.a.vargas@gmail.com>
2024-10-20 22:21:50 -0500

    Añadido los métodos  al productosController y clase controller

--- js/producto/productos.js
+++ js/producto/productos.js
@@ -1,27 +1,45 @@
 class Producto {
-    constructor () {
-        // TODO inicializar firestore y settings
+    constructor() {
+      // TODO inicializar firestore y settings
+      this.db = firebase.firestore();
+    }
   
-        this.db = firebase.firestore();
+    crearProducto(uid, emailUser, nombre, tipo, stock) {
+      return this.db.collection('productos').add({
+        uid: uid,
+        autor: emailUser,
+        nombre: nombre,
+        tipo: tipo,
+        stock: stock,
+        fecha: firebase.firestore.FieldValue.serverTimestamp()
+      }).then(refDoc => {
+        console.log(`Id del producto => ${refDoc.id}`);
+      }).catch(error => {
+        console.error(`Error creando el producto => ${error}`);
+      });
     }
 
+    actualizarProducto(idProducto, nombre, tipo, stock) {
+        return this.db.collection('productos').doc(idProducto).update({
+          nombre: nombre,
+          tipo: tipo,
+          stock: stock,
+          fechaActualizacion: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(() => {
+          console.log(`Producto actualizado`);
+        }).catch(error => {
+          console.error(`Error actualizando el producto => ${error}`);
+        });
+      }
 
-    crearProducto(uid, emailUser, nombre, stock, tipo){
-        return this.db.collection('productos').add({
-            uid: uid,
-            autor: emailUser,
-            nombre: nombre,
-            stock: stock,
-            tipo: tipo,
-            fecha: firebase.firestore.FieldValue.serverTimestamp()
-        }).then ( refdoc => {
-
-            console.log(`Id del producto => ${refDoc.id}`)
-        }).catch( error => {
-    
-            console.error(`Error creando el  producto => ${error}`)
-        })
-    }
-
-
+      borrarProducto(idProducto) {
+        return this.db.collection('productos').doc(idProducto).delete()
+          .then(() => {
+            console.log(`Producto borrado`);
+          })
+          .catch(error => {
+            console.error(`Error borrando el producto => ${error}`);
+          });
+      }
 }
+  
--- js/producto/productosController.js
+++ js/producto/productosController.js
@@ -1,43 +1,70 @@
 $(() => {
 
-    $('#btnNuevo').click(()=>{
+  // botón para crear un nuevo producto
+  $('#btnNuevo').click((event) => {
+    event.preventDefault(); // Prevenir que el formulario se envíe de forma tradicional
+    
+    const producto = new Producto();
+    const user = firebase.auth().currentUser;
+    if (user == null) {
+      alert(`Para crear el producto debes estar autenticado`);
+      window.location.replace('index.html');
+      return;
+    }
+    const nombre = $('#nombre').val();
+    const tipo = $('#tipo').val();
+    const stock = $('#stock').val();
+    producto.crearProducto(user.uid, user.email, nombre, tipo, stock)
+      .then(resp => {
+        alert(`Producto creado correctamente`);
+        window.location.replace('main.html');
+      })
+      .catch(err => {
+        console.log(`Error => ${err}`);
+        alert(`Error => ${err}`);
+      });
+  });
 
-        const producto = new Producto()
+  $('#btnVolver').click(() => {
+    window.location.replace('main.html'); // Redirigir a la página principal
+  });
 
-        const user = firebase.auth().currentUser
-        if (user == null) {
-          alert(`Para crear el producto debes estar autenticado`)
-          window.location.replace('index.html');
-          return
-        }
-
-        const nombre = $('#nombre').val();
-        const tipo = $('#tipo').val();
-        const stock = $('#stock').val();
-
-        producto.crearProducto(
-            user.uid,
-            user.email,
-            nombre,
-            tipo,
-            stock
-        ).then(resp => {
-            alert(`Producto creado correctamente`)
-            window.location.replace('main.html')
-          })
-          .catch(err => {
-            console.log(`Error => ${err}`)
-            alert(`Error => ${err}`)
-          })
-
+    // boton para actulizar  
+    $('#btnActualizar').click(() => {
+      const producto = new Producto();
+      const idProducto = $('#idProducto').val();
+      const nombre = $('#nombre').val();
+      const tipo = $('#tipo').val();
+      const stock = $('#stock').val();
+      producto.actualizarProducto(idProducto, nombre, tipo, stock)
+        .then(() => {
+          alert(`Producto actualizado correctamente`);
+          window.location.replace('main.html');
+        })
+        .catch(err => {
+          console.log(`Error => ${err}`);
+          alert(`Error => ${err}`);
+        });
     });
 
+    $('#btnEditar').click((event) => {
+      const idProducto = $(event.target).data('id');
+      window.location.replace(`editar.html?id=${idProducto}`);
+    });
 
-
-
-
-
-
+    $('#btnBorrar').click(() => {
+      const producto = new Producto();
+      const idProducto = $('#idProducto').val();
+      producto.borrarProducto(idProducto)
+        .then(() => {
+          alert(`Producto borrado correctamente`);
+          window.location.replace('main.html');
+        })
+        .catch(err => {
+          console.log(`Error => ${err}`);
+          alert(`Error => ${err}`);
+        });
+    });
 
 
 })
